fac13e346211a4ddb282038ee188f73739a5b1ff
Phodal HUANG <h@phodal.com>
2019-06-05 12:52:27 +0800

    stage1: parser copy

--- poc/stage1/src/parser.js
+++ poc/stage1/src/parser.js
@@ -25,6 +25,19 @@ const escapes = {
 
 const passEscapes = ['"', '\\', '/'];
 
+const OBJECT_STATES = {
+  _START_: 0,
+  OPEN_OBJECT: 1,
+  PROPERTY: 2,
+  COMMA: 3
+};
+
+const PROPERTY_STATES = {
+  _START_: 0,
+  KEY: 1,
+  COLON: 2
+};
+
 /** @param hexCode {string} hexCode without '\u' prefix */
 function parseHexEscape(hexCode) {
   let charCode = 0;
@@ -34,6 +47,59 @@ function parseHexEscape(hexCode) {
   }
 
   return String.fromCharCode(charCode);
+}
+
+
+function parseProperty(tokenList, index) {
+  // property: STRING COLON value
+  let startToken;
+  const property = {
+    type: 'Property',
+    key: null,
+    value: null
+  };
+  let state = PROPERTY_STATES._START_;
+
+  while (index < tokenList.length) {
+    const token = tokenList[index];
+
+    switch (state) {
+      case PROPERTY_STATES._START_: {
+        if (token.type === TOKEN_TYPES.STRING) {
+          const key = {
+            type: 'Identifier',
+            value: token.value,
+            raw: token.value
+          };
+          startToken = token;
+          property.key = key;
+          state = PROPERTY_STATES.KEY;
+          index ++;
+        } else {
+          return null;
+        }
+        break;
+      }
+
+      case PROPERTY_STATES.KEY: {
+        if (token.type === TOKEN_TYPES.COLON) {
+          state = PROPERTY_STATES.COLON;
+          index ++;
+        }
+        break;
+      }
+
+      case PROPERTY_STATES.COLON: {
+        const value = parseValue(tokenList, index);
+        property.value = value.value;
+        return {
+          value: property,
+          index: value.index
+        };
+      }
+
+    }
+  }
 }
 
 function parseString(/** string */string) {
@@ -68,7 +134,7 @@ function parseLiteral(tokenList, index) {
 
   switch (token.type) {
     case TOKEN_TYPES.STRING: {
-      value = parseString(input.slice(token.loc.start.offset + 1, token.loc.end.offset - 1));
+      value = token.value;
       break;
     }
     case TOKEN_TYPES.NUMBER: {
@@ -105,6 +171,67 @@ function parseLiteral(tokenList, index) {
 }
 
 function parseObject(tokenList, index) {
+  let startToken;
+  const object = {
+    type: 'Object',
+    children: []
+  };
+  let state = OBJECT_STATES._START_;
+
+  while (index < tokenList.length) {
+    const token = tokenList[index];
+
+    switch (state) {
+      case OBJECT_STATES._START_: {
+        if (token.type === TOKEN_TYPES.LEFT_BRACE) {
+          startToken = token;
+          state = OBJECT_STATES.OPEN_OBJECT;
+          index ++;
+        } else {
+          return null;
+        }
+        break;
+      }
+
+      case OBJECT_STATES.OPEN_OBJECT: {
+        if (token.type === TOKEN_TYPES.RIGHT_BRACE) {
+          return {
+            value: object,
+            index: index + 1
+          };
+        } else {
+          const property = parseProperty(tokenList, index);
+          object.children.push(property.value);
+          state = OBJECT_STATES.PROPERTY;
+          index = property.index;
+        }
+        break;
+      }
+
+      case OBJECT_STATES.PROPERTY: {
+        if (token.type === TOKEN_TYPES.RIGHT_BRACE) {
+          return {
+            value: object,
+            index: index + 1
+          };
+        } else if (token.type === TOKEN_TYPES.COMMA) {
+          state = OBJECT_STATES.COMMA;
+          index ++;
+        }
+        break;
+      }
+
+      case OBJECT_STATES.COMMA: {
+        const property = parseProperty(tokenList, index);
+        if (property) {
+          index = property.index;
+          object.children.push(property.value);
+          state = OBJECT_STATES.PROPERTY;
+        }
+        break;
+      }
+    }
+  }
 }
 
 const ARRAY_STATES = {
@@ -127,7 +254,6 @@ function parseArray(tokenList, index) {
   while (index < tokenList.length) {
     token = tokenList[index];
 
-    console.log(state, token.type);
     switch (state) {
       case ARRAY_STATES._START_: {
         if (token.type === TOKEN_TYPES.LEFT_BRACKET) {
@@ -182,7 +308,7 @@ function parseArray(tokenList, index) {
 function parseValue(tokenList, index) {
   let value = (
     parseLiteral(tokenList, index)
-    // || parseObject(tokenList, index)
+    || parseObject(tokenList, index)
     || parseArray(tokenList, index)
   );
 
