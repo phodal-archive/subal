93c32167aa0aa053b8254c55a26206b73dbbb5a3
Phodal HUANG <h@phodal.com>
2019-06-14 09:04:09 +0800

    generator: add first char to upper case

--- poc/stage1/src/generator.js
+++ poc/stage1/src/generator.js
@@ -1,3 +1,51 @@
+function buildArray(node) {
+  node = node[0];
+  switch (node.type) {
+    case 'Object': {
+      buildObject(node);
+      break;
+    }
+    case 'Literal':
+      console.log(node.value, typeof node.value);
+      break;
+  }
+}
+
+function uppercaseLetter(str) {
+  console.log(str, str.length);
+  if (str.charAt(0) === '"') {
+    str = str.substr(1);
+  }
+
+  if (str.charAt(str.length - 1) === '"') {
+    str = str.substr(0, str.length - 1);
+  }
+
+  str = str.charAt(0).toUpperCase() + str.slice(1);
+  if (str[str.length - 1] === 's') {
+    str = str.substr(0, str.length - 1);
+  }
+
+  return str;
+}
+
+function buildObject(node) {
+  for (let i = 0; i < node.children.length; i++) {
+    const childNode = node.children[i];
+    console.log(uppercaseLetter(childNode.key.value));
+    switch (childNode.value.type) {
+      case 'Array': {
+        buildArray([childNode.value.children[0]]);
+        break;
+      }
+      case 'Literal': {
+        console.log(childNode.value.value);
+        break;
+      }
+    }
+  }
+}
+
 function codeGenerator(originNode, node, res, result) {
   let firstNode = node[0];
 
@@ -30,19 +78,18 @@ function codeGenerator(originNode, node, res, result) {
   // Array
   if (firstNode.value && firstNode.value.children) {
     if (firstNode.value.children.length > 0) {
-      console.log(firstNode.key.value);
-      console.log(firstNode.type, firstNode.value.type);
+      buildArray(firstNode.value.children);
     }
-    return codeGenerator(originNode, firstNode.value.children, res, result);
+    // return codeGenerator(originNode, firstNode.value.children, res, result);
   }
 
   // Generator
   if (firstNode.value && firstNode.value.value) {
-    console.log(firstNode.key && firstNode.key.value, typeof firstNode.value.value);
-    console.log(firstNode.type, firstNode.value.type);
+    // console.log(firstNode.key && firstNode.key.value, typeof firstNode.value.value);
+    // console.log(firstNode.type, firstNode.value.type);
   } else {
-    console.log(firstNode.key && firstNode.key.value, typeof firstNode.value);
-    console.log(firstNode.type, firstNode.value.type);
+    // console.log(firstNode.key && firstNode.key.value, typeof firstNode.value);
+    // console.log(firstNode.type, firstNode.value.type);
   }
 
   res.pop();
