9091699e88e3118d621c569908a1a6bafd2ffd33
Phodal HUANG <h@phodal.com>
2019-06-19 22:17:19 +0800

    subal: init cli

---
+++ poc/stage1/cli.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const Subal = require('./index');
+
+if (process.argv.length < 3) {
+  console.log('Usage: node ' + process.argv[1] + ' FILENAME');
+  process.exit(1);
+}
+// Read the file and print its contents.
+let  filename = process.argv[2];
+fs.readFile(filename, 'utf8', function(err, data) {
+  if (err) throw err;
+  let subal = new Subal(data);
+
+  let results = subal
+    .tokenizer()
+    .parser()
+    .generator()
+    .renderString();
+
+  console.log(results);
+});
